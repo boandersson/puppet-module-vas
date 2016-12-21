58c6c00b77d1027aae1b2b2d0d5d00f7afaede72
Bosse Andersson <bandersson@hpe.com>
2016-12-21 13:59:23 +0100

    Add test to expose bug with puppet4
    If qas packages are installed, the vas_domain fact returns an empty
    string. puppet4 doesn't treat empty strings as false the same way as
    puppet3 does, causing the module to fail with "VAS version missmatch"

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -767,6 +767,23 @@ describe 'vas' do
       it { should_not contain_exec('vasinst') }
     end
 
+    context 'with realm set and vas not joined (vas_domain fact empty)' do
+      let :facts do
+        default_facts.merge(
+          {
+            :vas_domain => '',
+          }
+        )
+      end
+      let :params do
+        {
+          :realm      => 'realm.example.com',
+        }
+      end
+      it { should contain_class('vas') }
+      it { should contain_exec('vasinst') }
+    end
+
     # Domain change spec tests
     context 'with domain_change set to false and matching domains' do
       let :params do
